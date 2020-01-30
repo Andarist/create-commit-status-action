9a69f68981c60e1c3cdfea993787b6cb60a82f07
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-01-30 09:36:04 +0100

    Use local fork of universal-user-agent

---
+++ universal-user-agent/index.js
@@ -0,0 +1,15 @@
+const osName = require('os-name')
+
+function getUserAgentNode() {
+  try {
+    return `Node.js/${process.version.substr(1)} (${osName()}; ${process.arch})`
+  } catch (error) {
+    if (/wmic os get Caption/.test(error.message)) {
+      return 'Windows <version undetectable>'
+    }
+
+    throw error
+  }
+}
+
+module.exports = { getUserAgent: getUserAgentNode }
